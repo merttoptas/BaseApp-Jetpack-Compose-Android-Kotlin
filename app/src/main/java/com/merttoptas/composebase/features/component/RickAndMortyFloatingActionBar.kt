f63546c48a04f8176f09bf2d655ee4a03a35bacc
Mert Toptaş <merttoptas34@gmail.com>
2022-03-19 22:38:12 +0300

    Floating action create animate.

--- app/src/main/java/com/merttoptas/composebase/features/component/RickAndMortyFloatingActionBar.kt
+++ app/src/main/java/com/merttoptas/composebase/features/component/RickAndMortyFloatingActionBar.kt
@@ -1,16 +1,18 @@
 package com.merttoptas.composebase.features.component
 
 import androidx.compose.animation.animateColorAsState
-import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.FloatingActionButton
 import androidx.compose.material.Icon
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import kotlinx.coroutines.delay
 
 /**
  * Created by merttoptas on 12.03.2022
@@ -21,19 +23,39 @@ fun RickAndMortyFloatingActionBar(
     navController: NavController,
 ) {
     var isClick by remember { mutableStateOf(false) }
-    val rotationalState by animateFloatAsState(targetValue = if (isClick) 180f else 0f)
-    val animatedColor by animateColorAsState(targetValue = if (isClick) Color.Green else Color.Red)
+    val animatedColor by animateColorAsState(targetValue = if (isClick) Color.Green else Color.White)
+    val infiniteTransition = rememberInfiniteTransition()
+
+    LaunchedEffect(isClick) {
+        if (isClick) {
+            delay(800)
+            isClick = false
+        }
+    }
+
+    val favoriteSize by infiniteTransition.animateFloat(
+        initialValue = 24.0f,
+        targetValue = 48.0f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(
+                durationMillis = 800,
+                delayMillis = 100,
+                easing = FastOutLinearInEasing
+            ),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
 
     FloatingActionButton(
-        onClick = { isClick = !isClick },
+        onClick = { isClick = true },
         contentColor = Color.Transparent,
         backgroundColor = animatedColor
     ) {
         Icon(
             Icons.Filled.Favorite,
             contentDescription = null,
-            tint = Color.White,
-            modifier = Modifier.rotate(rotationalState)
+            tint = Color.Red,
+            modifier = Modifier.size(if (isClick) favoriteSize.dp else 24.dp)
         )
     }
 }
