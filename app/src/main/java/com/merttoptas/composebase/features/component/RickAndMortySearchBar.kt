fa443f0b685530e10ce9feb348601540bc6285ed
Mert Toptaş <merttoptas34@gmail.com>
2022-04-09 21:32:40 +0300

    SearchBar and textfield component added.

---
+++ app/src/main/java/com/merttoptas/composebase/features/component/RickAndMortySearchBar.kt
@@ -0,0 +1,66 @@
+package com.merttoptas.composebase.features.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.merttoptas.composebase.R
+
+/**
+ * Created by merttoptas on 9.04.2022
+ */
+
+@Composable
+fun RickAndMortySearchBar(
+    modifier: Modifier = Modifier,
+    text: String,
+    onTextChange: (text: String) -> Unit,
+    onClickSearch: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .shadow(elevation = 10.dp)
+    ) {
+        RickAndMortyTextField(
+            value = text,
+            onValueChange = { onTextChange(it) },
+        )
+        Box(
+            Modifier
+                .background(
+                    color = MaterialTheme.colors.onPrimary,
+                    shape = RoundedCornerShape(14.dp)
+                )
+                .size(56.dp)
+                .clickable { onClickSearch() }
+                .align(Alignment.CenterEnd),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_baseline_search_24),
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewMinimaSearchBar() {
+    RickAndMortySearchBar(text = "", onTextChange = {}, onClickSearch = {})
+}
---
+++ app/src/main/java/com/merttoptas/composebase/features/component/RickAndMortyTextField.kt
@@ -0,0 +1,68 @@
+package com.merttoptas.composebase.features.component
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+/**
+ * Created by merttoptas on 9.04.2022
+ */
+
+@Composable
+fun RickAndMortyTextField(
+    modifier: Modifier = Modifier,
+    value: String,
+    onValueChange: (String) -> Unit,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    placeholder: @Composable (() -> Unit)? = null,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions(),
+    singleLine: Boolean = true
+) {
+    TextField(
+        modifier = modifier
+            .height(56.dp).fillMaxWidth(),
+        value = value,
+        onValueChange = onValueChange,
+        shape = RoundedCornerShape(14.dp),
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = MaterialTheme.colors.background,
+            textColor = MaterialTheme.colors.primary,
+            cursorColor = MaterialTheme.colors.primary,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            errorIndicatorColor = Color.Transparent
+        ),
+        singleLine = singleLine,
+        trailingIcon = trailingIcon,
+        visualTransformation = visualTransformation,
+        keyboardActions = keyboardActions,
+        keyboardOptions = keyboardOptions,
+        placeholder = placeholder
+    )
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BodyPreview() {
+    RickAndMortyTextField(
+        modifier = Modifier,
+        value = "Value",
+        onValueChange = {},
+    )
+}
+
