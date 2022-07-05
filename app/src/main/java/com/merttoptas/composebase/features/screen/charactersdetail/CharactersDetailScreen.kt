6287c78905f12ea5810022b334de5563bfbac91d
Mert Toptaş <merttoptas34@gmail.com>
2022-07-05 22:37:13 +0300

    gradle update.

--- app/src/main/java/com/merttoptas/composebase/features/screen/charactersdetail/CharactersDetailScreen.kt
+++ app/src/main/java/com/merttoptas/composebase/features/screen/charactersdetail/CharactersDetailScreen.kt
@@ -6,10 +6,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
